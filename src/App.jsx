90f4d0d26b6a0ecc6afde67fea92752c0cdfa99c
Juan Pablo Suarez <jp.juansuarez@gmail.com>
2024-10-10 13:47:15 -0300

    Added: General responsivness for Text, Images, Buttons, Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,36 @@ import "./App.css";
 
 function App() {
   return (
-    <>
+    <div className="container">
     <div className="title">
       <h1> Refleja el éxito que has alcanzado con contenido auténtico e impactante </h1>
       <p>Tu marca tiene una historia que contar, ideamos y producimos el material audiovisual que necesitas para conectar con tu audiencia.</p>
       <button>Reserva tu reunión gratis</button>
       </div>
+      <h2>SOMOS TU EQUIPO IDEAL</h2>
       <div className="container_1">
-        <img src="/src/assets/images/img_1.png" /> 
-        <h2>SOMOS TU EQUIPO IDEAL</h2>
-        <ul>Tus redes</ul>
-        <li>Tus redes no reflejan la calidad de tu servicio o producto</li>
-        <li>No se entiende lo que vendes</li>
-        <li>No logras que te perciban como autoridad en tu nicho.</li>
-        <li>No sabes como llevar a cabo todas las ideas que tienes.</li>
+        <div className="imageContainer">
+          <img src="/src/assets/images/img_1.png" /> 
+        </div>
+
+        <div className="textContainer">
+          <ul>Tus redes</ul>
+          <li>Tus redes no reflejan la calidad de tu servicio o producto</li>
+          <li>No se entiende lo que vendes</li>
+          <li>No logras que te perciban como autoridad en tu nicho.</li>
+          <li>No sabes como llevar a cabo todas las ideas que tienes.</li>
+        </div>
+
       </div>
       <div className="container_2">
         <img src="/src/assets/images/img_2.png" /> 
+        <div className="textContainer">
         <ul>Y deseas</ul>
         <li>Verte como un referente y/o autoridad</li>
         <li>Producción de video</li>
         <li>Post-producción</li>
         <li>Creación de contenido para redes sociales</li>
+        </div>
       </div>
       <h3>Conoce como puedo ayudarte</h3>
       <button>Agenda tu reunion Gratiuta de Diagnostico</button>
@@ -107,7 +115,7 @@ function App() {
         <p>© 2024 Raiza Aviles Inc.</p>
         <p>Todos los derechos reservados</p>
       </footer>
-      </>
+      </div>
   );
 }
 
