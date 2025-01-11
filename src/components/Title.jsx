dcd3e93fc0aaa450198c50431b6b0771775bfe35
Juan Pablo Suarez <jp.juansuarez@gmail.com>
2025-01-10 23:28:17 -0300

    Added: Title component with styling for background and text elements

---
+++ src/components/Title.jsx
@@ -0,0 +1,12 @@
+import "./Title.css";
+
+const Title = () => (
+    <div className="title">
+        <div className="background"></div>
+        <h3>RAIZA AVILES INC</h3>
+        <h1>Refleja el éxito que has alcanzado</h1>
+        <p>PRODUCIMOS EL CONTENIDO AUTÉNTICO E IMPACTANTE QUE NECESITA TU MARCA PERSONAL</p>
+    </div>
+);
+
+export default Title;
