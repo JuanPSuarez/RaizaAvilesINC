ca7613eb2847584ee3fec25ae5f6d68cd2f0ede5
Juan Pablo Suarez <jp.juansuarez@gmail.com>
2025-01-10 23:27:03 -0300

    Added: Component "ServiceNeeds"

---
+++ src/components/ServiceNeeds.jsx
@@ -0,0 +1,33 @@
+import "./ServiceNeeds.css";
+
+const ServiceNeeds = () => (
+    <div style={{ backgroundColor: "var(--secondary-contrast-color)" }}>
+        <h2>PUEDES NECESITARNOS</h2>
+        <div className="container_1">
+            <div className="imageContainer">
+                <img src="/assets/images/img_1.png" />
+            </div>
+            <div className="textContainer">
+                <h2>Si...</h2>
+                <li>Tus redes no reflejan la calidad de tu servicio o producto</li>
+                <li>No se entiende lo que vendes</li>
+                <li>No logras que te perciban como autoridad en tu nicho.</li>
+                <li>No sabes como llevar a cabo todas las ideas que tienes.</li>
+            </div>
+        </div>
+        <div className="container_2">
+            <img src="/assets/images/img_2.jpg" />
+            <div className="textContainer">
+                <h2>Y deseas</h2>
+                <li>Verte como un referente y/o autoridad</li>
+                <li>Producción de video</li>
+                <li>Post-producción</li>
+                <li>Creación de contenido para redes sociales</li>
+            </div>
+        </div>
+        <h3>Conoce como puedo ayudarte</h3>
+    </div>
+
+);
+
+export default ServiceNeeds;
