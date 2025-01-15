88845e573ff6477740bc6cde5445b5f2e156e705
Juan Pablo Suarez <jp.juansuarez@gmail.com>
2025-01-14 21:08:33 -0300

    Updated: Enhanced ClientLogoGrid component layout with new container styles and increased logo count

--- src/components/ClientLogoGrid.jsx
+++ src/components/ClientLogoGrid.jsx
@@ -7,7 +7,7 @@ const ClientLogoGrid = () => {
   useEffect(() => {
     const loadLogos = () => {
       const logoList = [];
-      const totalLogos = 10; 
+      const totalLogos = 11;
 
       for (let i = 1; i <= totalLogos; i++) {
         logoList.push(`/assets/logos/Logo_${i}.png`); // Update the naming pattern if necessary
@@ -20,13 +20,17 @@ const ClientLogoGrid = () => {
   }, []);
 
   return (
-    <div className="gridContainer">
-      {logos.map((logo, index) => (
-        <div key={index} className='logoContainer'>
-          <img src={logo} alt={`Client logo ${index}`} className='logo' />
-        </div>
-      ))}
+    <div className="clientContianer">
+      <h2>CONFIAN EN NOSOTROS</h2>
+      <div className="gridContainer">
+        {logos.map((logo, index) => (
+          <div key={index} className='logoContainer'>
+            <img src={logo} alt={`Client logo ${index}`} className='logo' />
+          </div>
+        ))}
+      </div>
     </div>
+
   );
 };
 
