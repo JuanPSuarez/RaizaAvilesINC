a07d49cbf91556d9f7e852187e71673f8da565a9
Juan Pablo Suarez <jp.juansuarez@gmail.com>
2025-01-14 21:08:54 -0300

    Updated: Enhanced Title component styles with new image display properties

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -5,6 +5,7 @@ const Title = () => (
         <div className="background"></div>
         <h3>RAIZA AVILES INC</h3>
         <h1 className="heroTitle">Refleja el éxito que has alcanzado</h1>
+        {/* <img src="/assets/images/bg_0.gif  " alt="hero" /> */}
         <p>PRODUCIMOS EL CONTENIDO AUTÉNTICO E IMPACTANTE QUE NECESITA TU MARCA PERSONAL</p>
     </div>
 );
