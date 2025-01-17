01219c23a8a9cff86b872cefed6f4208b3a02121
Juan Pablo Suarez <jp.juansuarez@gmail.com>
2025-01-16 22:39:14 -0300

    Updated: Title component styles for improved layout and structure

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,11 +2,14 @@ import "./Title.css";
 
 const Title = () => (
     <div className="title">
-        <div className="background"></div>
-        <h3>RAIZA AVILES INC</h3>
-        <h1 className="heroTitle">Refleja el éxito que has alcanzado</h1>
-        {/* <img src="/assets/images/bg_0.gif  " alt="hero" /> */}
-        <p>PRODUCIMOS EL CONTENIDO AUTÉNTICO E IMPACTANTE QUE NECESITA TU MARCA PERSONAL</p>
+        <div className="background">
+            <h3>RAIZA AVILES INC</h3>
+            <div className="heroTitle">
+                <h1>Refleja el éxito que has alcanzado</h1>
+                <p>PRODUCIMOS EL CONTENIDO AUTÉNTICO E IMPACTANTE QUE NECESITA TU MARCA PERSONAL</p>
+                <img src="/assets/images/bg_0.gif  " alt="hero" />
+            </div>
+        </div>
     </div>
 );
 
