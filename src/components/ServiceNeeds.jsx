c3c01be1178e9fe35cf936236dfbbf361e28d10f
Juan Pablo Suarez <jp.juansuarez@gmail.com>
2025-01-23 01:21:47 -0300

    Updated: refined styling for ServiceNeeds component with box shadow, adjusted width, and improved text presentation for clarity and engagement

--- src/components/ServiceNeeds.jsx
+++ src/components/ServiceNeeds.jsx
@@ -2,17 +2,17 @@ import "./ServiceNeeds.css";
 
 const ServiceNeeds = () => (
     <div className="serviceNeeds">
-        <h2>PUEDES NECESITARNOS</h2>
+        {/* <h2>PUEDES NECESITARNOS</h2> */}
         <div className="container_1">
             <div className="imageContainer">
                 <img src="/assets/images/img_1.jpg" />
             </div>
             <div className="textContainer">
-                <h2>Si...</h2>
+                <h2>SI TE PASA QUE:</h2>
                 <li>Tus redes no reflejan la calidad de tu servicio o producto</li>
-                <li>No se entiende lo que vendes</li>
                 <li>No logras que te perciban como autoridad en tu nicho.</li>
-                <li>No sabes como llevar a cabo todas las ideas que tienes.</li>
+                <li>No sabes como llevar a cabo todas las ideas que tienes</li>
+                <li>No tienes tiempo para crear contenido</li>
             </div>
         </div>
         <div className="container_2">
@@ -20,17 +20,18 @@ const ServiceNeeds = () => (
                 <img src="/assets/images/img_2.jpg" />
             </div>
             <div className="textContainer">
-                <h2>Y deseas</h2>
-                <li>Verte como un referente y/o autoridad</li>
-                <li>Producción de video</li>
-                <li>Post-producción</li>
-                <li>Creación de contenido para redes sociales</li>
+                <h2>Y DESEAS:</h2>
+                <li>Verte como un referente en tu nicho</li>
+                <li>Delegar para escalar</li>
+                <li>Aumentar tu credibidilidad</li>
+                <li>Elevar tu valor percibido</li>
             </div>
         </div>
-        {/* <h3>Conoce como puedo ayudarte</h3>
-        <a href="https://api.whatsapp.com/send?phone=5493517872051&text=¡Hola!+quiero+saber+mas+sobre+tus+servicios">
-            <button>Contáctanos</button>
-        </a> */}
+        <h2>ENTONCES SOMOS LA AGENCIA PARA TI</h2>
+        <h3>Agenda una llamada gratuita de diagnostico</h3>
+        <a href="https://calendly.com/raizaavilesinc">
+            <button>Agendar llamada</button>
+        </a>
     </div>
 
 );
