f591ef61922c5a7b9ac3c217dfae0605af68369b
Juan Pablo Suarez <jp.juansuarez@gmail.com>
2025-02-05 18:21:13 -0300

    Added: Bio component with styled content and image, including italicized text for improved readability.

---
+++ src/components/Bio.jsx
@@ -0,0 +1,19 @@
+import "./Bio.css";
+
+const Bio = () => (
+    <div className="bio">
+        <h1>MI HISTORIA</h1>
+        <img src="/assets/images/img_10.jpg" alt="bio" />
+        <div className="bio-content">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultriciessed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonumm
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultriciessed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonumm
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultriciessed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonumm
+
+            </p>
+            <h2>Lorem ipsum dolor sit amet</h2>
+        </div>
+    </div>
+);
+
+export default Bio;
