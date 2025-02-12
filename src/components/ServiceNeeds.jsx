999ed5631fc7a0014c5e975a4256d077c90af231
Juan Pablo Suarez <jp.juansuarez@gmail.com>
2025-02-12 01:17:39 -0300

    Updated: improved accessibility by adding alt text to images and structured lists in ServiceNeeds component

--- src/components/ServiceNeeds.jsx
+++ src/components/ServiceNeeds.jsx
@@ -4,20 +4,24 @@ import "./ServiceNeeds.css";
 const ServiceNeeds = () => (
     <div className="serviceNeeds">
         <HeroImage
-            imageUrl="/assets/images/img_1.jpg"
+            imageUrl="/assets/images/img_1.jpg" alt="service needs, raiza aviles"
         />
         <h2>SI TE PASA QUE:</h2>
-        <li>Tus redes no reflejan la calidad de tu servicio o producto</li>
-        <li>No logras que te perciban como autoridad en tu nicho.</li>
-        <li>No sabes como llevar a cabo todas las ideas que tienes</li>
-        <li>No tienes tiempo para crear contenido</li>
-        <img src="/assets/images/img_2.png" />
+        <ul>
+            <li>Tus redes no reflejan la calidad de tu servicio o producto</li>
+            <li>No logras que te perciban como autoridad en tu nicho.</li>
+            <li>No sabes como llevar a cabo todas las ideas que tienes</li>
+            <li>No tienes tiempo para crear contenido</li>
+        </ul>
+        <img src="/assets/images/img_2.jpg" alt="service needs, raiza aviles" />
         <h2>Y DESEAS:</h2>
-        <li>Verte como un referente en tu nicho</li>
-        <li>Delegar para escalar</li>
-        <li>Aumentar tu credibidilidad</li>
-        <li>Elevar el valor percibido de tu marca.</li>
-        <h2>ENTONCES ESTE SERVICIO ES PARA TI</h2>
+        <ul>
+            <li>Verte como un referente en tu nicho</li>
+            <li>Delegar para escalar</li>
+            <li>Aumentar tu credibidilidad</li>
+            <li>Elevar el valor percibido de tu marca.</li>
+            <h2>ENTONCES ESTE SERVICIO ES PARA TI</h2>
+        </ul>
         <a href="https://calendly.com/raizaavilesinc">
             <button>Agendar llamada</button>
         </a>
