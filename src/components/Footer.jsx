c3aa2148f41d11df00f6b686dc6b5d421f5ab2b7
Juan Pablo Suarez <jp.juansuarez@gmail.com>
2025-02-12 01:17:51 -0300

    Updated: added alt text to social media icons in Footer component for improved accessibility

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,19 +7,19 @@ const Footer = () => (
         <footer>
             <div className="footericons">
                 <a href="https://www.facebook.com/raizaavilesinc">
-                    <img src="/assets/icons/icon_1.png" />
+                    <img src="/assets/icons/icon_1.png" alt="facebook logo" />
                 </a>
                 <a href="https://www.instagram.com/raizaaviles.inc">
-                    <img src="/assets/icons/icon_2.png" />
+                    <img src="/assets/icons/icon_2.png" alt="instagram logo" />
                 </a>
                 <a href="https://api.whatsapp.com/send?phone=%2B543517872051">
-                    <img src="/assets/icons/icon_3.png" />
+                    <img src="/assets/icons/icon_3.png" alt="whatsapp logo" />
                 </a>
                 <a href="https://www.youtube.com/@raizaavilesinc">
-                    <img src="/assets/icons/icon_4.png" />
+                    <img src="/assets/icons/icon_4.png" alt="youtube logo" />
                 </a>
                 <a href="https://www.tiktok.com/@raiza_aviles?_t=ZM-8tqAagKGrRe&_r=1">
-                    <img src="/assets/icons/icon_5.png" />
+                    <img src="/assets/icons/icon_5.png" alt="tiktok logo" />
                 </a>
             </div>
             <p>© {new Date().getFullYear()} Raiza Aviles Inc.</p>
