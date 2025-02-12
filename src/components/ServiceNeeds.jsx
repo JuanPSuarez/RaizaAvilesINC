a6c07d663303b7b762b8fcdd69f2cb86b3a6a57a
Juan Pablo Suarez <jp.juansuarez@gmail.com>
2025-02-11 23:09:25 -0300

    Updated: refactored ServiceNeeds component layout and styles for improved structure and readability, integrating HeroImage component

--- src/components/ServiceNeeds.jsx
+++ src/components/ServiceNeeds.jsx
@@ -1,34 +1,23 @@
+import HeroImage from "./HeroImage";
 import "./ServiceNeeds.css";
 
 const ServiceNeeds = () => (
     <div className="serviceNeeds">
-        {/* <h2>PUEDES NECESITARNOS</h2> */}
-        <div className="container_1">
-            <div className="imageContainer">
-                <img src="/assets/images/img_1.jpg" />
-            </div>
-            <div className="textContainer">
-                <h2>SI TE PASA QUE:</h2>
-                <li>Tus redes no reflejan la calidad de tu servicio o producto</li>
-                <li>No logras que te perciban como autoridad en tu nicho.</li>
-                <li>No sabes como llevar a cabo todas las ideas que tienes</li>
-                <li>No tienes tiempo para crear contenido</li>
-            </div>
-        </div>
-        <div className="container_2">
-            <div className="imageContainer">
-                <img src="/assets/images/img_2.jpg" />
-            </div>
-            <div className="textContainer">
-                <h2>Y DESEAS:</h2>
-                <li>Verte como un referente en tu nicho</li>
-                <li>Delegar para escalar</li>
-                <li>Aumentar tu credibidilidad</li>
-                <li>Elevar tu valor percibido</li>
-            </div>
-        </div>
-        <h2>ENTONCES SOMOS LA AGENCIA PARA TI</h2>
-        <h3>Agenda una llamada gratuita de diagnostico</h3>
+        <HeroImage
+            imageUrl="/assets/images/img_1.jpg"
+        />
+        <h2>SI TE PASA QUE:</h2>
+        <li>Tus redes no reflejan la calidad de tu servicio o producto</li>
+        <li>No logras que te perciban como autoridad en tu nicho.</li>
+        <li>No sabes como llevar a cabo todas las ideas que tienes</li>
+        <li>No tienes tiempo para crear contenido</li>
+        <img src="/assets/images/img_2.png" />
+        <h2>Y DESEAS:</h2>
+        <li>Verte como un referente en tu nicho</li>
+        <li>Delegar para escalar</li>
+        <li>Aumentar tu credibidilidad</li>
+        <li>Elevar el valor percibido de tu marca.</li>
+        <h2>ENTONCES ESTE SERVICIO ES PARA TI</h2>
         <a href="https://calendly.com/raizaavilesinc">
             <button>Agendar llamada</button>
         </a>
