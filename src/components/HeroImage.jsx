d305b9c16881a39797edc5f21fe3625fec5178b6
Juan Pablo Suarez <jp.juansuarez@gmail.com>
2025-02-12 01:16:55 -0300

    Updated: enhanced HeroImage component with responsive margin adjustments and improved alt text for accessibility

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -1,19 +1,16 @@
 import "./HeroImage.css";
 import PropTypes from "prop-types";
 
-HeroImage.propTypes = {
-    imageUrl: PropTypes.string.isRequired,
-};
-HeroImage.defaultProps = {
-    imageUrl: "./assets/images/img_0.jpg",
-};
-export default function HeroImage( {imageUrl} ) {
-
+export default function HeroImage({ imageUrl = "./assets/images/img_0.jpg" }) {
     return (
         <div className="hero-img">
             <div className="hero-img-container">
-                <img className="img-hero" src={imageUrl} alt="" />
+                <img className="img-hero" src={imageUrl} alt="Raiza Aviles Image" />
             </div>
         </div>
     );
 }
+
+HeroImage.propTypes = {
+    imageUrl: PropTypes.string,
+};
